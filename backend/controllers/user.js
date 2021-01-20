015c9d3392f1e2a7ef23dc8292f86c19a0a8cd9c
charlotte amiot <charlotte.amiot@gmail.com>
2021-01-20 12:29:02 +0100

    Création du controller pour auth/login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -28,6 +28,20 @@ exports.login = (req,re,next) => {
     user => {
       if(!user){
         return res.status(401).json({message: 'Utilisateur non trouvé !'});
+      }else{
+        bcrypt.compare(req.body.password, user.password)
+        .then( 
+          valid => {
+            if(!valid){
+                return res.status(401).json({ message: 'Mot de passe incorrect !'});
+            }else{
+              res.status(200).json({ 
+                userId: user._id,
+                token: 'TOKEN'
+              });
+            }
+          })
+        .catch(error => res.status(500).json({error}));
       }
     }
   )

c332d4bef8a84ba319d3b9c2959a155208ba3ca0
charlotte amiot <charlotte.amiot@gmail.com>
2021-01-20 09:17:23 +0100

    modification du modèle de données user

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    userId: { type: String, required: true},
     email: { type: String, required: true, unique:true},
-    password: { type: String, required: true},
+    password: { type: String, required: true}
 })
 
 userSchema.plugin(uniqueValidator);
